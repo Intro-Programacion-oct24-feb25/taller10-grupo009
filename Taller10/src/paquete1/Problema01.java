d957b4992cc0896a9ba9502d76abe5fe4799af7b
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-12-19 12:34:51 -0500

    cambios ejercicios

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -14,7 +14,38 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double[][] notas = {{9, 10, 7, 8}, 
+                            {10, 5, 7, 9}, 
+                            {7, 9.4, 10, 9}};
+        String[] nombresEstudiante = {"Jerry Ponce", "Gabriela lewis"
+                , "David Bell"};
+        double[] sumaNotas = new double[3];
+        double[] promedio = new double[3];
+                            
+        double suma = 0;
+        String reporte = "";
+        
+        
+        for (int i = 0; i < notas.length; i++) { 
+            suma = 0;
+            for (int j = 0; j < notas[i].length; j++) { 
+                suma = suma + notas[i][j];
+                     
+            }
+            sumaNotas[i] = suma;
+            promedio[i] = suma/ 4;// 
+        }
+        
+        // Presentación de resultados
+        for (int i = 0; i < nombresEstudiante.length; i++) {
+            reporte = String.format("%sEstudiante: %s tiene un "
+                    + "promedio de %.2f\n",
+                    reporte,
+                    nombresEstudiante[i],
+                    promedio[i]);
+        }
+        
+        System.out.printf("%s\n", reporte);
+        
     }
-    
 }
